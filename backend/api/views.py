57dbd2fbb2d50a5dba1a97e1f2fd2d609169d15c
Abdur Rahman Akash <akash22205101195@diu.edu.bd>
2025-02-08 00:28:11 +0600

    Add Serializer.py File

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
+from django.conf import settings
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.http import urlsafe_base64_encode
+from django.utils.encoding import force_bytes
+from django.db.models import Sum
 
-# Create your views here.
+# Rest Framework
+from rest_framework import status
+from rest_framework.decorators import api_view, APIView
+from rest_framework.response import Response
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.decorators import permission_classes, api_view
+from rest_framework_simplejwt.tokens import RefreshToken
+
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
+from datetime import datetime
+
+# other
+import json
+import random
+
+# Custom Imports
+from api import models as api_models
+from api import serializers as api_serializers
+
